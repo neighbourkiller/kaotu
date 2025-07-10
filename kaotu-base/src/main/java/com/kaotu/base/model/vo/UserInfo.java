fa9678413e5b836cf15b750c403b83c7a85dced5
neighbourkiller <3319578099@qq.com>
2025-07-10 09:06:47 +0800

    user修改

--- kaotu-base/src/main/java/com/kaotu/base/model/vo/UserInfo.java
+++ kaotu-base/src/main/java/com/kaotu/base/model/vo/UserInfo.java
@@ -2,11 +2,13 @@ package com.kaotu.base.model.vo;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class UserInfo {
     private String userId;
-    private String userName;
+    private String username;
     private String email;
-    private String unblockTime;
-    private String lastLogin;
+    private LocalDateTime unblockTime;
+    private LocalDateTime lastLogin;
 }
